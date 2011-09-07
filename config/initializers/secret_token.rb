cbe3b8b419b1aa124ee16c8d4b06a5b07119b960
Dominic Sayers <dominic@sayers.cc>
2011-09-07 17:17:58 +0100

    for StackOverflow question

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Momkid::Application.config.secret_token = '82f98fd11a63f6b81a43d932036320ae9500d242e4e46134ad2cc417c0997b03188a5bf42d4654af90a54d4f891436c53674efc63069861544c5f8bda05a2f40'
