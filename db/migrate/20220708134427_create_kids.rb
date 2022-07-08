bf906d5030cd81467819c3511394a3254e522018
Dominic Sayers <dominic@sayers.cc>
2022-07-08 14:44:46 +0100

    `rails g model Kid name:string mom:references`

---
+++ db/migrate/20220708134427_create_kids.rb
@@ -0,0 +1,10 @@
+class CreateKids < ActiveRecord::Migration[7.0]
+  def change
+    create_table :kids do |t|
+      t.string :name
+      t.references :mom, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
