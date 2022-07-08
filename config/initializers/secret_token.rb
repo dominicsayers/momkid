65ccf996bab9437b997105c5773024ee859f55fa
Dominic Sayers <dominic@sayers.cc>
2022-07-08 15:29:25 +0100

    Remove hard-coded secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Momkid::Application.config.secret_token = '82f98fd11a63f6b81a43d932036320ae9500d242e4e46134ad2cc417c0997b03188a5bf42d4654af90a54d4f891436c53674efc63069861544c5f8bda05a2f40'
+Momkid::Application.config.secret_token = REDACTED
