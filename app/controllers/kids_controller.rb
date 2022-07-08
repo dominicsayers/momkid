ffcce4cf555a55cab69a448565b0fa784b5ac731
Dominic Sayers <dominic@sayers.cc>
2022-07-08 14:50:04 +0100

    Add methods to KidsController
    Still following the instructions here:
    https://www.dominicsayers.com/howto-create-a-simple-parent-child-form-in-rails-3-1/

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,2 +1,14 @@
 class KidsController < ApplicationController
+  def create
+    @mom = Mom.find(params[:mom_id])
+    @kid = @mom.kids.create(params.require(:kid).permit(:name))
+    redirect_to mom_path(@mom)
+  end
+
+  def destroy
+    @mom = Mom.find(params[:mom_id])
+    @kid = @mom.kids.find(params[:id])
+    @kid.destroy
+    redirect_to mom_path(@mom)
+  end
 end
