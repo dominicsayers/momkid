9dcde263a37b95f3771d6960b43b8c6dc2d06030
Dominic Sayers <dominic@sayers.cc>
2024-11-21 10:21:08 +0000

    chore: `rubocop -A`

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class KidsController < ApplicationController
   def create
     @mom = Mom.find(params[:mom_id])
--- app/models/mom.rb
+++ app/models/mom.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class Mom < ApplicationRecord
   has_many :kids, dependent: :destroy
-  accepts_nested_attributes_for :kids, allow_destroy: :true
+  accepts_nested_attributes_for :kids, allow_destroy: true
 end
